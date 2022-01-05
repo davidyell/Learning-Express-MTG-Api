adcba3f74a7566ef76cd6d0efb0b1099ec45fe87
David Yell <david.yell@comparisontech.com>
2022-01-05 17:17:03 +0000

    Tidy up

--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const deck = {
-    id: 1,
-    name: 'Radford Ellenburg - Alabama States',
-    cards: [
-      '1 Shinka, the Bloodsoaked Keep',
-      '1 Eiganjo Castle',
-      '3 City of Brass',
-      '3 Mountain',
-      '12 Plains',
-      '4 Samurai of the Pale Curtain',
-      '1 Nagao, Bound by Honor',
-      '3 Kami of Ancient Law',
-      '3 Hearth Kami',
-      '1 Eight-and-a-Half-Tails',
-      '4 Savannah Lions',
-      '4 Leonin Skyhunter',
-      '1 Masako the Humorless',
-      '2 Isamaru, Hound of Konda',
-      '4 Lantern Kami',
-      '4 Electrostatic Bolt',
-      '3 Otherworldly Journey',
-      '2 Raise the Alarm',
-      '4 Glorious Anthem',
-      'Sideboard',
-      '1 Mountain',
-      '2 Auriok Champion',
-      '2 Sacred Ground',
-      '3 Shatter',
-      '3 Karma',
-      '4 Guerrilla Tactics',
-    ]
-};
+router.get('/', function (request, response, next) {
+    response.send('<h1>This is api only</h1>');
+    response.end();
+});
 
 router.get('/api/decks', function(request, response, next) {
+    // TODO: Implement some database lookup
     response.json({
       decks: [
-          deck
+          allDecks
       ]
-  })
+    })
 });
 
 router.get('/api/decks/:id', function(request, response, next) {
