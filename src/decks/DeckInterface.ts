fff0850d9ccfdfabd52cb9efc1787ae34eecea0d
David Yell <neon1024@gmail.com>
2022-08-19 11:29:05 +0100

    Refactoring. Remove redundant files.

--- src/decks/DeckInterface.ts
+++
@@ -1,5 +0,0 @@
-export default interface Deck {
-    id: number,
-    name: string,
-    cards: string | string[]
-};
--- src/decks/cardListConversion.ts
+++
@@ -1,8 +0,0 @@
-const listToArray = (cards: string): string[] => cards.split(', ');
-
-const arrayToList = (cards: string[]): string => cards.join(', ');
-
-export {
-  listToArray,
-  arrayToList,
-};
