997792bf35afd7a2a16b2b413205d8bd4283f22d
David Yell <david.yell@mdgroup.com>
2022-09-07 10:08:55 +0100

    Fixed a bug
    When throwing from Prisma this would halt the web server process and prevent further connections, so added a try catch to handle returning a correct 404 response

--- src/controllers/decks.controller.ts
+++ src/controllers/decks.controller.ts
@@ -19,31 +19,40 @@ const index = async (request: Request, response: Response) => {
 };
 
 const view = async (request: Request, response: Response) => {
-  const result = await prismaClient.decks.findUniqueOrThrow({
-    where: { id: parseInt(request.params.id, 10) },
-    include: {
-      player: true,
-      cards_in_decks: {
-        include: {
-          cards: {
-            select: {
-              id: true,
-              name: true,
-              colorIdentity: true,
-              convertedManaCost: true,
-              manaCost: true,
-              manaValue: true,
-              rarity: true,
-              subtypes: true,
-              supertypes: true,
-              type: true,
-              power: true,
-              toughness: true,
-              text: true,
+  try {
+    const result = await prismaClient.decks.findUniqueOrThrow({
+      where: { id: parseInt(request.params.id, 10) },
+      include: {
+        player: true,
+        cards_in_decks: {
+          include: {
+            cards: {
+              select: {
+                id: true,
+                name: true,
+                colorIdentity: true,
+                convertedManaCost: true,
+                manaCost: true,
+                manaValue: true,
+                rarity: true,
+                subtypes: true,
+                supertypes: true,
+                type: true,
+                power: true,
+                toughness: true,
+                text: true,
+              },
             },
           },
         },
       },
+    });
+
+    response.json(result);
+  } catch (error) {
+    response.status(404).json({ error: 'No deck found' });
+  }
+};
     },
   });
 
