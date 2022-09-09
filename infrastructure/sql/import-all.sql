44eb863a2007405a37a9219adf93b51081f52327
David Yell <neon1024@gmail.com>
2022-09-09 20:53:13 +0100

    Do not import all the tables data as it's not being used. Updated the readme to inject data as a single command, like the workflow. Removed the database from the repo, as it can now be generated fully in about 2-3 mins

--- infrastructure/sql/import-all.sql
+++ infrastructure/sql/import-all.sql
@@ -1,6 +1,6 @@
-.read ./infrastructure/sql/_sets_.sql
+-- .read ./infrastructure/sql/_sets_.sql
 .read ./infrastructure/sql/cards.sql
-.read ./infrastructure/sql/legalities.sql
-.read ./infrastructure/sql/meta.sql
-.read ./infrastructure/sql/rulings.sql
-.read ./infrastructure/sql/tokens.sql
+-- .read ./infrastructure/sql/legalities.sql
+-- .read ./infrastructure/sql/meta.sql
+-- .read ./infrastructure/sql/rulings.sql
+-- .read ./infrastructure/sql/tokens.sql
