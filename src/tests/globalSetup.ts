f7194625d9e96482c7ad5fe5b1a05340344a8a23
David Yell <david.yell@mdgroup.com>
2022-09-22 16:53:46 +0100

    Documented testing setup
    Created some shorthand scripts to split the tests by suite, so running unit suite can avoid setting up a db

--- src/tests/globalSetup.ts
+++ src/tests/globalSetup.ts
@@ -1,8 +1,2 @@
-/* eslint-disable no-console */
-
-const setup = async () => {
-  console.log('> Creating testing database...');
-  // TODO: Run a Prisma migration file, which includes just the card data required
-  // TODO: Remove the github actions step of building the database
-};
+const setup = async () => { };
 export default setup;
--- src/tests/globalTeardown.ts
+++ src/tests/globalTeardown.ts
@@ -1,7 +1,3 @@
-import fsPromise from 'node:fs/promises';
-
-const teardown = async () => {
-  await fsPromise.unlink(process.env.DATABASE_URL as string);
-};
+const teardown = async () => { };
 
 export default teardown;
