974b82891b6410fff9105cbb517c7c2a7f3be0ff
David Yell <david.yell@mdgroup.com>
2022-09-22 09:41:04 +0100

    Remove the verbose, and change tack with populating the database

--- src/tests/globalSetup.ts
+++ src/tests/globalSetup.ts
@@ -1,15 +1,8 @@
 /* eslint-disable no-console */
-import util from 'node:util';
-
-const execFile = util.promisify(require('node:child_process').execFile);
 
 const setup = async () => {
   console.log('> Creating testing database...');
-  await execFile('./infrastructure/setup-db.sh', [], (error: Error, stdout: string | Buffer, stderr: string | Buffer) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  });
+  // TODO: Run a Prisma migration file, which includes just the card data required
+  // TODO: Remove the github actions step of building the database
 };
 export default setup;
